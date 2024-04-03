bc1b9ba16f7a486061ce1a7525e7628cdfc95a80
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-03 15:02:24 +0300

    commit

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-});
-
-
 var users = require('./routes/users');
 
 var app = express();
