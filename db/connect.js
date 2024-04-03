4c2c749271c2579c33e35e726d984b429ec8db4c
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-03 14:41:15 +0300

    COMMIT

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,17 @@
    
 const dotenv = require('dotenv');
 dotenv.config();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
 
 let _db;
 
